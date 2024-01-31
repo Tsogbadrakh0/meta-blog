173680b65cef5afa2fb21e2563ee22100fbc0f94
tsogbadrakh baatar <23LP4804@iMAC2021-05s-iMac.local>
2024-01-31 11:03:15 +0800

    Pinecone

---
+++ src/components/Highlight.jsx
@@ -0,0 +1,27 @@
+export const Highlight = (props) => {
+    const { highlights } = props;
+  
+    return (
+      <div className="mb-[100px] max-w-[100vw] relative">
+        {highlights.map((el) => (
+          <div>
+            <img
+              src={el.social_image}
+              className="rounded-xl w-[1216px] h-[600px]"
+            />
+            <div className="rounded-xl w-[598px] p-[40px] bg-[#fff] absolute left-2 bottom-2">
+              <h2 className="mb-[16px] bg-indigo-500 flex justify-center rounded-md text-[#fff] py-[4px] w-[97px] text-sm leading-tight">
+                Technology
+              </h2>
+              <p className="pb-[24px] text-gray-900 text-2xl font-semibold">
+                {el.title}
+              </p>
+              <p className="text-neutral-400 text-base leading-normal">
+                August 20, 2022
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
---
+++ src/components/Trending.jsx
@@ -0,0 +1,29 @@
+export const Trending = (props) => {
+    const { trendings } = props;
+  
+    return (
+      <div className="pb-[100px]">
+        <h2 className="leading-7 font-bold pb-[30px] text-[24px]">Trending</h2>
+        <div className="flex gap-[20px]">
+          {trendings.map((el) => (
+            <div className=" relative">
+              <div>
+                <img
+                  src={el.cover_image}
+                  className=" w-[289px] h-[320px] rounded-xl"
+                />
+                <div className="absolute bottom-6 left-6">
+                  <h2 className=" mb-[16px] bg-indigo-500 flex justify-center rounded-md text-[#fff] py-[4px] w-[86px] text-sm leading-tight">
+                    Technology
+                  </h2>
+                  <p className="font-semibold w-[230px] text-[#fff] text-lg">
+                    {el.title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
